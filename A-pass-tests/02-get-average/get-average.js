2e6d51ca33685a451f0905cf6242eade59e6d05f
farzad-nazif <farzadnazif.nz@gmail.com>
2022-11-07 19:05:19 +0000

    A-pass-tests done

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,16 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let sum = 0;
+  let count = 0;
+  numbers.forEach((item) => {
+    if (typeof item === "number") {
+      sum += item;
+      count++;
+    }
+  });
+  return sum / count;
+}
 
 module.exports = average;
--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,22 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let result = {
+    Ford: 0,
+    Honda: 0,
+    "Land Rover": 0,
+    Toyota: 0,
+  };
+  carsSold.forEach((car) => {
+    if (car.make === "Ford") {
+      result.Ford += car.price;
+    } else if (car.make === "Honda") {
+      result.Honda += car.price;
+    } else if (car.make === "Land Rover") {
+      result["Land Rover"] += car.price;
+    } else if (car.make === "Toyota") {
+      result.Toyota += car.price;
+    }
+  });
+  return result;
+}
 
 module.exports = sales;
